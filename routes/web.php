cc05362df24fe8383f63dede68cbdf8b913f3a8d
sun <sunxuejia834@dingtalk.com>
2020-03-13 17:40:30 +0800

    修改访问log包含范围

--- routes/web.php
+++ routes/web.php
@@ -14,39 +14,40 @@
 //Route::get('/', function () {
 //    return view('welcome');
 //});
-Route::match(['get','post'],'login','Admin\IndexController@login');
+Route::match(['get', 'post'], 'login', 'Admin\IndexController@login');
 
-Route::group(['middleware'=>'logvisitor'],function(){
+Route::group(['middleware' => 'logvisitor'], function () {
 
-    Route::any('device','Device\DeviceauthorizeController@index');
+    Route::any('device', 'Device\DeviceauthorizeController@index');
 
-    Route::any('imgrecevice','Home\IndexController@imgRecevice');
-});
+    Route::any('imgrecevice', 'Home\IndexController@imgRecevice');
+//});
 
 
-Route::group(['middleware'=>'checkadminlogin'],function(){
+    Route::group(['middleware' => 'checkadminlogin'], function () {
 
 //    Route::get('/','Home\IndexController@index');
-    Route::get('/','Home\IndexController@tableshow');
-    Route::get('/tableshow','Home\IndexController@tableshow');
-    Route::get('/tableshow/company/{id}','Home\IndexController@showCompany');
-    Route::get('/tableshow/company/editItem/{id}','Home\IndexController@edita');
-    Route::post('/tableshow/company/editItem/','Home\IndexController@edita');
+        Route::get('/', 'Home\IndexController@tableshow');
+        Route::get('/tableshow', 'Home\IndexController@tableshow');
+        Route::get('/tableshow/company/{id}', 'Home\IndexController@showCompany');
+        Route::get('/tableshow/company/editItem/{id}', 'Home\IndexController@edita');
+        Route::post('/tableshow/company/editItem/', 'Home\IndexController@edita');
 
 //    Route::get('/tableshow/addDevice','Home\IndexController@addDevice');
 //    Route::post('/tableshow/addDevice','Home\IndexController@addDevice');
-    Route::match(['get','post'],'/tableshow/addDevice','Home\IndexController@addDevice');
+        Route::match(['get', 'post'], '/tableshow/addDevice', 'Home\IndexController@addDevice');
 
 
 //    Route::get('/tableshow/addCompany','Home\IndexController@addCompany');
 //    Route::post('/tableshow/addCompany','Home\IndexController@addCompany');
-    Route::match(['get','post'],'/tableshow/addCompany','Home\IndexController@addCompany');
+        Route::match(['get', 'post'], '/tableshow/addCompany', 'Home\IndexController@addCompany');
 
-    Route::post('/tableshow/company/dela','Home\IndexController@dela');
-    Route::get('/additem','Home\IndexController@additem');
-    Route::get('/importexcel','Home\IndexController@importexcel');
+        Route::post('/tableshow/company/dela', 'Home\IndexController@dela');
+        Route::get('/additem', 'Home\IndexController@additem');
+        Route::get('/importexcel', 'Home\IndexController@importexcel');
 
-    Route::get('logout','Admin\IndexController@logout');
+        Route::get('logout', 'Admin\IndexController@logout');
+    });
 });
 
 
