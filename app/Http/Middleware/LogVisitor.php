771e02617a4db6b8676eebb8b0f73166a2713270
sun <sunxuejia834@dingtalk.com>
2020-03-23 10:06:07 +0800

    修改前置log中间件为后置log中间件，捕获服务器返回值和返回状态

--- app/Http/Middleware/LogVisitor.php
+++ app/Http/Middleware/LogVisitor.php
@@ -17,13 +17,34 @@ class LogVisitor
     public function handle($request, Closure $next)
     {
 
-        $data = [
-            'ip' => $request->getClientIp(),
-            'url' => $request->path(),
-            'method' => $request->getRealMethod(),
-            'content' => json_encode($request->all()),
-        ];
+//        $data = [
+//            'ip' => $request->getClientIp(),
+//            'url' => $request->path(),
+//            'method' => $request->getRealMethod(),
+//            'content' => json_encode($request->all()),
+//        ];
+//        Log::info($data);
+//        return $next($request);
+        $response = $next($request);
+        if ($request->path() == 'device' or $request->path() == 'imgrecevice'){
+            $data = [
+                'ip' => $request->getClientIp(),
+                'url' => $request->path(),
+                'method' => $request->getRealMethod(),
+                'request_content' => json_encode($request->all()),
+                'response_content' => $response->content(),
+                'response_statuscode' => $response->status(),
+            ];
+        }else{
+            $data = [
+                'ip' => $request->getClientIp(),
+                'url' => $request->path(),
+                'method' => $request->getRealMethod(),
+                'request_content' => json_encode($request->all()),
+                'response_statuscode' => $response->status(),
+            ];
+        }
         Log::info($data);
-        return $next($request);
+        return $response;
     }
 }
