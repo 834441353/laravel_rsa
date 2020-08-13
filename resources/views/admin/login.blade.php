a1a1307fa7fe25524de7b682ab301713431bf2e2
root <834441353@qq.com>
2020-08-13 10:16:00 +0800

    添加备案号

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -95,6 +95,12 @@ MAIN CONTENT
 
     </div>
 </div>
+<footer class="site-footer navbar-fixed-bottom">
+    <div class="text-center">
+        <a href="http://www.beian.miit.gov.cn/" target="_blank"> 粤ICP备19101562号</a>
+
+    </div>
+</footer>
 
 <!-- js placed at the end of the document so the pages load faster -->
 <script src="{{ asset('index') }}/assets/js/jquery.js"></script>
